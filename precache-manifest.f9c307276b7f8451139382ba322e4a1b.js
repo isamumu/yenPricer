951b046fdfae1c391e1e402d12c9808822dde786
isamumu <isamu510@hotmail.com>
2024-12-29 22:59:24 +0900

    Deploy React app to GitHub Pages

---
+++ precache-manifest.f9c307276b7f8451139382ba322e4a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f3089fdbb06a254b31509f727d94c2",
+    "url": "/yenPricer/index.html"
+  },
+  {
+    "revision": "0b84cfa95a455440598c",
+    "url": "/yenPricer/static/css/main.4e218a7b.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/yenPricer/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/yenPricer/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "0b84cfa95a455440598c",
+    "url": "/yenPricer/static/js/main.ac5812f4.chunk.js"
+  },
+  {
+    "revision": "ae9641d44d79908dcbd2",
+    "url": "/yenPricer/static/js/runtime~main.10c08ee1.js"
+  }
+]);
